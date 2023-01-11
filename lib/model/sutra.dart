3fdc9e567b5cf720d0e7aef9ef5f94ff5abaeea8
souliya pps <souliya.litd@gmail.com>
2023-01-11 16:54:01 +0700

    Navbar & Zoom

---
+++ lib/model/sutra.dart
@@ -0,0 +1,20 @@
+import 'package:hive/hive.dart';
+
+part 'sutra.g.dart';
+
+@HiveType(typeId: 0)
+class Sutra extends HiveObject {
+  @HiveField(0, defaultValue: 0)
+  final int id;
+  @HiveField(1)
+  final String title;
+  @HiveField(2)
+  final String content;
+  @HiveField(3)
+  final String category;
+  Sutra(
+      {required this.id,
+      required this.title,
+      required this.content,
+      required this.category});
+}
---
+++ lib/model/sutra.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sutra.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SutraAdapter extends TypeAdapter<Sutra> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Sutra read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Sutra(
+      id: fields[0] == null ? 0 : fields[0] as int,
+      title: fields[1] as String,
+      content: fields[2] as String,
+      category: fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Sutra obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.content)
+      ..writeByte(3)
+      ..write(obj.category);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SutraAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
