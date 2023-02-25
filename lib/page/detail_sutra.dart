28a464797216c929e96f0eaed5cf76229d94ae99
souliya pps <souliya.litd@gmail.com>
2023-02-25 16:45:06 +0700

    detail style

--- lib/page/detail_sutra.dart
+++ lib/page/detail_sutra.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lao_tipitaka/main.dart';
 
-// how to create DetailSutra page
 class DetailSutra extends StatefulWidget {
   const DetailSutra({
     Key? key,
@@ -9,6 +8,7 @@ class DetailSutra extends StatefulWidget {
     required this.content,
     required this.category,
   }) : super(key: key);
+
   final String title;
   final String content;
   final String category;
@@ -17,7 +17,48 @@ class DetailSutra extends StatefulWidget {
   State<DetailSutra> createState() => _DetailSutraState();
 }
 
-class _DetailSutraState extends State<DetailSutra> {
+class _DetailSutraState extends State<DetailSutra>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _scaleController;
+  late Animation<double> _scaleAnimation;
+  double _scale = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scaleController =
+        AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
+    _scaleAnimation = Tween<double>(begin: _scale, end: _scale).animate(_scaleController)
+      ..addListener(() {
+        setState(() {
+          _scale = _scaleAnimation.value;
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _scaleController.dispose();
+    super.dispose();
+  }
+
+  void _onScaleStart(ScaleStartDetails details) {
+    _scaleController.stop();
+  }
+
+  void _onScaleUpdate(ScaleUpdateDetails details) {
+    setState(() {
+      _scale = _scaleAnimation.value;
+      _scale *= details.scale;
+    });
+  }
+
+  void _onScaleEnd(ScaleEndDetails details) {
+    _scaleAnimation = Tween<double>(begin: _scale, end: 1.0).animate(_scaleController);
+    _scaleController.reset();
+    _scaleController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +67,16 @@ class _DetailSutraState extends State<DetailSutra> {
         backgroundColor: const Color.fromARGB(241, 179, 93, 78),
       ),
       drawer: const NavigationDrawer(),
-      body: InteractiveViewer(
-        maxScale: 4.0,
-        minScale: 0.5,
-        child: Container(
-          color: Colors.orangeAccent[100],
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ListView(
+      body: GestureDetector(
+        onScaleStart: _onScaleStart,
+        onScaleUpdate: _onScaleUpdate,
+        onScaleEnd: _onScaleEnd,
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.orangeAccent[100],
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Text(
                   widget.title,
@@ -42,41 +85,32 @@ class _DetailSutraState extends State<DetailSutra> {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                  height: 1,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  textAlign: TextAlign.start,
-                  widget.content,
-                  style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+                const SizedBox(height: 10),
+                const Divider(color: Colors.black, thickness: 1, height: 1),
+                const SizedBox(height: 10),
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Container(
+                    constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height,
+                    ),
+                    child: Text(
+                      widget.content,
+                      style: TextStyle(
+                        fontSize: 20 * _scale,
+                        height: 1.5,
+                      ),
+                    ),
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                  height: 1,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
+                const SizedBox(height: 10),
+                const Divider(color: Colors.black, thickness: 1, height: 1),
+                const SizedBox(height: 10),
                 Text(
-                  textAlign: TextAlign.right,
                   widget.category,
-                  style: const TextStyle(
-                    fontSize: 20,
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontSize: 20 * _scale,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
