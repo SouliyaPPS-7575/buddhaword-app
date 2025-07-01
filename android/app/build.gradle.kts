8488f363d6bcc894d85264a1ce22218043aa85e0
SouliyaPPS-7575 <souliyapps@gmail.com>
2025-07-02 01:37:50 +0700

    🐞 fix(version code):

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 55
+        versionName = "55.0.0+1"
     }
 
     signingConfigs {
